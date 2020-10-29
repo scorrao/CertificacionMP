7273eb86508a622eb24f2cdbfc1d0935ffa5e53d
#Vincent <santiagocorrao96@outlook.com>
2020-10-29 16:35:05 -0300

    Resultado Satisfactorio

--- success.php
+++ success.php
@@ -1,3 +1,9 @@
 <?php
 var_dump($_GET);
+
+echo "payment_id: " . $_GET['payment_id'];
+
+echo "collection_id: " . $_GET['collection_id'];
+echo "external_reference: " . $_GET['external_reference'];
+echo "payment_method_id : " . $_GET['payment_method_id'];
 ?>
